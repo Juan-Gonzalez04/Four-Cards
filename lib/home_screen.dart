6fc4a1921ff23183074691d8d426e14e634df314
Juan Gonzalez <juanpgonzalez4002@mi.unc.edu.ar>
2025-04-22 23:52:41 -0300

    Avanzo en Home_Screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,18 +4,40 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Bienvenido a DASH'),
-        backgroundColor: Colors.deepPurple,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {
-              print("Ajustes presionados");
+      body: Center(
+        child:
+        
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () {
+                print("PANTALLA JUGAR");
+                //LLAMAR A JUGAR
             },
-          ),
-        ],
+            child: Text('JUGAR'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                print("PANTALLA CARRERA");
+                //LLAMAR A MODO CARRERA
+            },
+            child: Text('CARRERA'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                print("PANTALLA OPCIONES");
+                //LLAMAR A OPCIONES
+            },
+            child: Text('OPCIONES'),
+            ),
+          ],
       ),
+      )
     );
   }
 }
